e1eeeee54fc18b8c13367678ec2b7fe4a2d594bf
admin <carwolt123@gmail.com>
2023-03-06 21:18:27 +0500

    Initial Commit

---
+++ LocationRequest SwiftUI/LocationRequest_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationRequest_SwiftUIApp.swift
+//  LocationRequest SwiftUI
+//
+//  Created by iMac G1 on 06/03/23.
+//
+
+import SwiftUI
+
+@main
+struct LocationRequest_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
